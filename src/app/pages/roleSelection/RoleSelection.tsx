faabbdd591ead8a656348390bbe549a3f5148b9d
ivan.sakoman <ivan.sakoman@reroot.hr>
2021-12-28 16:27:03 +0100

    added scroll and fix typography

--- src/app/pages/roleSelection/RoleSelection.tsx
+++ src/app/pages/roleSelection/RoleSelection.tsx
@@ -16,22 +16,30 @@ const RoleSelection: React.FC<Props> = (props) => {
                 <img src={heroImg} alt="Hero Img" />
             </div>
             <div className="role-selection__content">
-                <div className="mb-22"><img src={logo} alt="Theorem" /></div>
-                <div className="type--lg type--wgt--bold mb-4">Register</div>
-                <div className="mb-2">I want to register as a</div>
-                <div className="role-selection__form">
-                    {
-                        roleSelectionOptions.map((roleOption: IRoleSelectionOption) => {
-                            return <div className="role-selection__item" key={roleOption.id} onClick={() => alert(roleOption.id + ' - ' + roleOption.title)}>
-                                <img src={gradientCircle} alt="gradient circle" />
-                                <div className="flex--grow ml-4">
-                                    <div className="mb-1">{roleOption.title}</div>
-                                    <div className="type--color--secondary">{roleOption.description}</div>
+                <div className="flex--grow">
+                    <div className="mb-22"><img src={logo} alt="Theorem" /></div>
+                    <div className="type--lg type--wgt--bold mb-4">Register</div>
+                    <div className="mb-2">I want to register as a</div>
+                    <div className="role-selection__form">
+                        {
+                            roleSelectionOptions.map((roleOption: IRoleSelectionOption) => {
+                                return <div className="role-selection__item" key={roleOption.id} onClick={() => alert(roleOption.id + ' - ' + roleOption.title)}>
+                                    <img src={gradientCircle} alt="gradient circle" />
+                                    <div className="flex--grow ml-4">
+                                        <div className="mb-1">{roleOption.title}</div>
+                                        <div className="type--color--secondary">{roleOption.description}</div>
+                                    </div>
+                                    <i className="icon icon--base icon--arrow-right icon--primary"></i>
                                 </div>
-                                <i className="icon icon--base icon--arrow-right icon--primary"></i>
-                            </div>
-                        })
-                    }
+                            })
+                        }
+                    </div>
+                </div>
+                <div className="flex--primary w--448">
+                    <div>© Theorem</div>
+                    <div>
+                        Already have an account? <a href="/login">Log in</a>
+                    </div>
                 </div>
             </div>
         </div>
