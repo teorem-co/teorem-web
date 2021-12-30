3a533d83c6b2287ef992c9e9d957fb86411f192c
ivan.sakoman <ivan.sakoman@reroot.hr>
2021-12-30 10:23:54 +0100

    fixed calendar

---
+++ src/app/constants/bookingEvents.ts
@@ -0,0 +1,175 @@
+const myEventList = [
+    {
+        id: 1,
+        label: 'Ured struja',
+        title: '42000',
+        start: new Date(
+            'Tue Nov 16 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        end: new Date(
+            'Tue Nov 16 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        allDay: false,
+        className: '',
+    },
+    {
+        id: 9,
+        label: 'Nekakav inflow',
+        title: '42000',
+        start: new Date(
+            'Tue Nov 16 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        end: new Date(
+            'Tue Nov 16 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        allDay: false,
+        className: '',
+    },
+    {
+        id: 2,
+        label: 'Gaming oprema',
+        title: '-42000',
+        start: new Date(
+            'Tue Nov 16 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        end: new Date(
+            'Tue Nov 16 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        allDay: false,
+    },
+    {
+        id: 3,
+        label: 'Hrana',
+        title: '42000',
+        start: new Date(
+            'Tue Nov 16 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        end: new Date(
+            'Tue Nov 16 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        allDay: false,
+    },
+    {
+        id: 4,
+        label: 'Jana mjesecna naplata',
+        title: '-10000',
+        start: new Date(
+            'Wed Nov 17 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        end: new Date(
+            'Wed Nov 17 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        allDay: false,
+    },
+    {
+        id: 10,
+        label: 'Jana mjesecna naplata',
+        title: '-10000',
+        start: new Date(
+            'Wed Nov 17 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        end: new Date(
+            'Wed Nov 17 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        allDay: false,
+    },
+    {
+        id: 11,
+        label: 'Jana mjesecna naplata',
+        title: '10000',
+        start: new Date(
+            'Wed Nov 17 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        end: new Date(
+            'Wed Nov 17 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        allDay: false,
+    },
+    {
+        id: 12,
+        label: 'Jana mjesecna naplata',
+        title: '-10000',
+        start: new Date(
+            'Wed Nov 17 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        end: new Date(
+            'Wed Nov 17 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        allDay: false,
+    },
+    {
+        id: 13,
+        label: 'Jana mjesecna naplata',
+        title: '10000',
+        start: new Date(
+            'Wed Nov 17 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        end: new Date(
+            'Wed Nov 17 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        allDay: false,
+    },
+    {
+        id: 14,
+        label: 'Jana mjesecna naplata',
+        title: '10000',
+        start: new Date(
+            'Wed Nov 17 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        end: new Date(
+            'Wed Nov 17 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        allDay: false,
+    },
+
+    {
+        id: 5,
+        label: 'Marketing kampanja',
+        title: '-12356',
+        start: new Date(
+            'Tue Nov 16 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        end: new Date(
+            'Tue Nov 16 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        allDay: false,
+    },
+    {
+        id: 6,
+        label: 'Nove stolice',
+        title: '22000',
+        start: new Date(
+            'Thu Nov 18 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        end: new Date(
+            'Thu Nov 18 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        allDay: false,
+    },
+    {
+        id: 7,
+        label: 'Marketing kampanja',
+        title: '-12356',
+        start: new Date(
+            'Tue Nov 02 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        end: new Date(
+            'Tue Nov 02 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        allDay: false,
+    },
+    {
+        id: 8,
+        label: 'Nove stolice',
+        title: '22000',
+        start: new Date(
+            'Thu Nov 07 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        end: new Date(
+            'Thu Nov 07 2021 00:10:00 GMT+0100 (Central European Standard Time)'
+        ),
+        allDay: false,
+    },
+];
+
+export default myEventList;
