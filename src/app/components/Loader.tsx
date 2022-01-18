52be0830f76ba07708f6fb20f39268797a73993d
ivan.sakoman <ivan.sakoman@reroot.hr>
2022-01-18 13:02:03 +0100

    fixed sceleton loader

--- src/app/components/Loader.tsx
+++ src/app/components/Loader.tsx
@@ -2,25 +2,37 @@ import React from 'react';
 import ContentLoader from 'react-content-loader';
 
 const Loader = () => (
-    <ContentLoader
-        speed={2}
-        width={'100%'}
-        height={240}
-        viewBox="0 0 1399 240"
-        backgroundColor="#f3f3f3"
-        foregroundColor="#ecebeb"
-    >
-        <circle cx="114" cy="118" r="79" />
-        <circle cx="114" cy="118" r="68" />
-        <rect x="1065" y="39" rx="4" ry="4" width="129" height="23" />
-        <rect x="1065" y="73" rx="4" ry="4" width="129" height="23" />
-        <rect x="1065" y="165" rx="4" ry="4" width="195" height="28" />
-        <rect x="252" y="39" rx="4" ry="4" width="132" height="24" />
-        <rect x="252" y="70" rx="4" ry="4" width="222" height="21" />
-        <rect x="252" y="162" rx="4" ry="4" width="62" height="19" />
-        <rect x="321" y="162" rx="4" ry="4" width="62" height="19" />
-        <rect x="252" y="113" rx="4" ry="4" width="377" height="28" />
-    </ContentLoader>
+    <div className="flex--primary p-6">
+        <ContentLoader
+            className="mr-6"
+            speed={2}
+            width={594}
+            height={157}
+            viewBox="0 0 594 157"
+            backgroundColor="#f3f3f3"
+            foregroundColor="#ecebeb"
+        >
+            <circle cx="79" cy="79" r="79" />
+            <circle cx="79" cy="79" r="68" />
+            <rect x="217" y="0" rx="4" ry="4" width="132" height="24" />
+            <rect x="217" y="31" rx="4" ry="4" width="222" height="21" />
+            <rect x="217" y="123" rx="4" ry="4" width="62" height="19" />
+            <rect x="286" y="123" rx="4" ry="4" width="62" height="19" />
+            <rect x="217" y="74" rx="4" ry="4" width="377" height="28" />
+        </ContentLoader>
+        <ContentLoader
+            speed={2}
+            width={195}
+            height={154}
+            viewBox="0 0 195 154"
+            backgroundColor="#f3f3f3"
+            foregroundColor="#ecebeb"
+        >
+            <rect x="0" y="0" rx="4" ry="4" width="129" height="23" />
+            <rect x="0" y="34" rx="4" ry="4" width="129" height="23" />
+            <rect x="0" y="126" rx="4" ry="4" width="195" height="28" />
+        </ContentLoader>
+    </div>
 );
 
 export default Loader;
