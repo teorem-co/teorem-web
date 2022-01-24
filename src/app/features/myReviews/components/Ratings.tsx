7e8324565b8ac21e9bda9aaaf5b5abb239f42152
ivan.sakoman <ivan.sakoman@reroot.hr>
2022-01-24 10:54:05 +0100

    fixed ratings

--- src/app/features/myReviews/components/Ratings.tsx
+++ src/app/features/myReviews/components/Ratings.tsx
@@ -1,14 +1,41 @@
+import { FC, useEffect, useState } from 'react';
+
 import ITutorStatisticsResult from '../interfaces/ITutorStatisticsResult';
 
 interface Props {
     ratings: ITutorStatisticsResult[];
 }
-const Ratings = (props: Props) => {
+
+
+
+const Ratings: FC<Props> = (props: Props) => {
     const { ratings } = props;
+
+    const [statisticsPlaceholder, setStatisticsPlaceholder] = useState<ITutorStatisticsResult[]>([]);
+
+    useEffect(() => {
+        const placeholderArray: ITutorStatisticsResult[] = [];
+        //map array for grades which are not included in the array from the backend
+        for (let i = 0; i < 5; i++) {
+            const currentItem: ITutorStatisticsResult | undefined = ratings.find(x => x.mark === (i + 1));
+
+            if (currentItem) {
+                placeholderArray.push(currentItem);
+            } else {
+                const currentObj: ITutorStatisticsResult = {
+                    count: '0',
+                    mark: i + 1,
+                    perCent: 0
+                };
+                placeholderArray.push(currentObj);
+            }
+        }
+        setStatisticsPlaceholder(placeholderArray);
+    }, []);
 
     return (
         <div>
-            {ratings.map((item: ITutorStatisticsResult, index: number) => {
+            {statisticsPlaceholder.length > 0 ? statisticsPlaceholder.map((item: ITutorStatisticsResult, index: number) => {
                 return (
                     <div key={index} className="rating__item">
                         <div className="mr-3">{item.mark}&nbsp;stars</div>
@@ -25,7 +52,8 @@ const Ratings = (props: Props) => {
                         <div className="ml-3">({item.count})</div>
                     </div>
                 );
-            })}
+            }) : <></>
+            }
         </div>
     );
 };
