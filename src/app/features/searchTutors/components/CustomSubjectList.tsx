79aeecbf3dea7b199d3dbc08b0b8686e56a6c716
Ivan Rajkovača <ivan.rajkovaca@reroot.hr>
2022-01-25 14:09:08 +0100

    Finished custom subject list inside search tutor item.

---
+++ src/app/features/searchTutors/components/CustomSubjectList.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+import ISubject from '../../../../interfaces/ISubject';
+
+interface Props {
+    subjects: ISubject[];
+}
+
+const CustomSubjectList = (props: Props) => {
+    const { subjects } = props;
+
+    const [showTooltip, setShowTooltip] = useState(false);
+
+    return (
+        <div className="flex">
+            {subjects.slice(0, 3).map((subject) => (
+                <span className="tag tag--primary" key={subject.id}>
+                    {subject.name}
+                </span>
+            ))}
+
+            {subjects.length > 3 ? (
+                <div className="pos--rel">
+                    <span
+                        className="type--color--brand cur--pointer"
+                        onClick={() => setShowTooltip(!showTooltip)}
+                    >
+                        {`+ ${subjects.length - 3} more`}{' '}
+                    </span>
+                    <div
+                        className={`tooltip--text ${
+                            showTooltip ? 'active' : ''
+                        }`}
+                    >
+                        {subjects.slice(3).map((subject) => (
+                            <span
+                                className="type--color--white p-1"
+                                key={subject.id}
+                            >
+                                {subject.name}
+                            </span>
+                        ))}
+                    </div>
+                </div>
+            ) : (
+                <></>
+            )}
+        </div>
+    );
+};
+
+export default CustomSubjectList;
