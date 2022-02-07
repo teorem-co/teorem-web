460934f59e5b8579b3cf5b484f77349124af2bff
ivan.sakoman <ivan.sakoman@reroot.hr>
2022-02-07 16:41:49 +0100

    commented slider

--- src/app/features/landing/components/ImageSlider.tsx
+++ src/app/features/landing/components/ImageSlider.tsx
@@ -1,23 +1,23 @@
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 
-import { Carousel } from 'react-responsive-carousel';
-
+//import { Carousel } from 'react-responsive-carousel';
 import calendar from '../../../../assets/images/landing_calendar.jpg';
 import tutorList from '../../../../assets/images/landing_tutor_list.jpg';
 
 const ImageSlider = () => {
     return (
-        <Carousel
-            autoPlay={true}
-            infiniteLoop={true}
-            showIndicators={false}
-            showThumbs={false}
-            showStatus={false}
-            showArrows={false}
-            centerMode={true}
-            centerSlidePercentage={70}
-            className="landing__slider"
-        >
+        // <Carousel
+        //     autoPlay={true}
+        //     infiniteLoop={true}
+        //     showIndicators={false}
+        //     showThumbs={false}
+        //     showStatus={false}
+        //     showArrows={false}
+        //     centerMode={true}
+        //     centerSlidePercentage={70}
+        //     className="landing__slider"
+        // >
+        <>
             <div>
                 <img src={calendar} className="landing__img" alt="calendar" />
             </div>
@@ -27,7 +27,8 @@ const ImageSlider = () => {
             <div>
                 <img src={calendar} className="landing__img" alt="calendar" />
             </div>
-        </Carousel>
+        </>
+        // </Carousel>
     );
 };
 
