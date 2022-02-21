e7f14df332471b8ea51d75e980046c24957d5657
ivan.sakoman <ivan.sakoman@reroot.hr>
2022-02-21 15:10:16 +0100

    added isAccepted to Ibooking

--- src/app/features/my-bookings/interfaces/IBooking.ts
+++ src/app/features/my-bookings/interfaces/IBooking.ts
@@ -1,42 +1,19 @@
+import ILevel from '../../../../interfaces/ILevel';
+import ISubject from '../../../../interfaces/ISubject';
+import ITutor from '../../../../interfaces/ITutor';
+import IUser from '../../../../interfaces/IUser';
+
 export default interface IBooking {
     id: string;
-    Level: {
-        abrv: string;
-        id: string;
-        name: string;
-    };
-    Subject: {
-        abrv: string;
-        id: string;
-        name: string;
-        levelId: string;
-    };
-    Tutor: {
-        aboutLessons: string;
-        aboutTutor: string;
-        currentOccupation: string;
-        userId: string;
-        levelId: string;
-        yearsOfExperience: string;
-    };
-    User: {
-        countryId: null;
-        dateOfBirth: null;
-        email: string;
-        firstName: string;
-        id: string;
-        lastName: string;
-        parentId: null;
-        password: string;
-        passwordResetToken: string;
-        phoneNumber: null;
-        phonePrefix: null;
-        roleId: string;
-    };
+    Level: ILevel;
+    Subject: ISubject;
+    Tutor: ITutor;
+    User: IUser;
     tutorId: string;
     studentId: string;
     subjectId: string;
     levelId: string;
     startTime: string;
     endTime: string;
+    isAccepted: boolean;
 }
