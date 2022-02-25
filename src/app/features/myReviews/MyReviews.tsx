693a27b603309c3dbd41457b73b7e83b842466c9
ivan.sakoman <ivan.sakoman@reroot.hr>
2022-02-25 17:14:37 +0100

    fixed loader on myReviews

--- src/app/features/myReviews/MyReviews.tsx
+++ src/app/features/myReviews/MyReviews.tsx
@@ -10,10 +10,7 @@ import Ratings from './components/Ratings';
 import ReviewItem from './components/ReviewItem';
 import IMyReview from './interfaces/IMyReview';
 import IMyReviewParams from './interfaces/IMyReviewParams';
-import {
-    useLazyGetMyReviewsQuery,
-    useLazyGetStatisticsQuery,
-} from './services/myReviewsService';
+import { useLazyGetMyReviewsQuery, useLazyGetStatisticsQuery } from './services/myReviewsService';
 
 export interface IGetMyReviews {
     rpp: number;
@@ -23,54 +20,15 @@ export interface IGetMyReviews {
 
 const MyReviews = () => {
     const [params, setParams] = useState<IMyReviewParams>({ page: 1, rpp: 10 });
-
-    const [getMyReviews, { data: myReviews, isLoading: myReviewsLoading }] =
-        useLazyGetMyReviewsQuery();
-
     const [loadedReviews, setLoadedReviews] = useState<IMyReview[]>([]);
 
-    const [
-        getStatistics,
-        { data: tutorStatistics, isLoading: statisticsLoading },
-    ] = useLazyGetStatisticsQuery();
+    const [getMyReviews, { data: myReviews, isLoading: myReviewsLoading, isUninitialized: reviewsUninitialized }] = useLazyGetMyReviewsQuery();
+    const [getStatistics, { data: tutorStatistics, isLoading: statisticsLoading, isUninitialized: statisticsUninitialized }] =
+        useLazyGetStatisticsQuery();
 
     const tutorId = useAppSelector((state) => state.auth.user?.id);
-
-    useEffect(() => {
-        if (tutorId) {
-            const obj: IGetMyReviews = {
-                tutorId: tutorId,
-                page: params.page,
-                rpp: params.rpp,
-            };
-
-            getMyReviews(obj);
-            getStatistics(tutorId);
-        }
-    }, []);
-
-    useEffect(() => {
-        const currentReviews = cloneDeep(loadedReviews);
-        if (myReviews) {
-            setLoadedReviews(currentReviews.concat(myReviews.rows));
-        }
-    }, [myReviews]);
-
-    useEffect(() => {
-        if (tutorId) {
-            const obj: IGetMyReviews = {
-                tutorId: tutorId,
-                page: params.page,
-                rpp: params.rpp,
-            };
-            getMyReviews(obj);
-        }
-    }, [params]);
-
-    // change paginated number
-    // const paginate = (pageNumber: number) => {
-    //     setParams({ page: pageNumber, rpp: params.rpp });
-    // };
+    const debouncedScrollHandler = debounce((e) => handleScroll(e), 500);
+    const isLoading = myReviewsLoading || reviewsUninitialized || statisticsLoading || statisticsUninitialized;
 
     const handleLoadMore = () => {
         let newParams = { ...params };
@@ -93,8 +51,6 @@ const MyReviews = () => {
         return returnValue;
     };
 
-    const debouncedScrollHandler = debounce((e) => handleScroll(e), 500);
-
     const handleScroll = (e: HTMLDivElement) => {
         const innerHeight = e.scrollHeight;
         const scrollPosition = e.scrollTop + e.clientHeight;
@@ -108,27 +64,53 @@ const MyReviews = () => {
         // }
     };
 
+    const fetchData = async () => {
+        if (tutorId) {
+            const obj: IGetMyReviews = {
+                tutorId: tutorId,
+                page: params.page,
+                rpp: params.rpp,
+            };
+
+            await getMyReviews(obj).unwrap();
+            await getStatistics(tutorId).unwrap();
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        const currentReviews = cloneDeep(loadedReviews);
+        if (myReviews) {
+            setLoadedReviews(currentReviews.concat(myReviews.rows));
+        }
+    }, [myReviews]);
+
+    useEffect(() => {
+        if (tutorId) {
+            const obj: IGetMyReviews = {
+                tutorId: tutorId,
+                page: params.page,
+                rpp: params.rpp,
+            };
+            getMyReviews(obj);
+        }
+    }, [params]);
+
     return (
         <MainWrapper>
-            <div
-                onScroll={(e: any) => debouncedScrollHandler(e.target)}
-                className="card--secondary"
-            >
+            <div onScroll={(e: any) => debouncedScrollHandler(e.target)} className="card--secondary">
                 <div className="card--secondary__head">
-                    <h2 className="type--wgt--bold type--lg">
-                        {t('MY_REVIEWS.TITLE')}
-                    </h2>
+                    <h2 className="type--wgt--bold type--lg">{t('MY_REVIEWS.TITLE')}</h2>
                 </div>
                 <div className="card--secondary__body my-reviews__wrapper">
                     <div className="my-reviews__main">
                         <div className="mb-10 flex--primary">
                             <div>
-                                <span className="type--uppercase type--color--tertiary">
-                                    {t('MY_REVIEWS.COUNT_TITLE')}
-                                </span>
-                                <span className="tag--primary d--ib ml-2">
-                                    {myReviews ? myReviews.count : 0}
-                                </span>
+                                <span className="type--uppercase type--color--tertiary">{t('MY_REVIEWS.COUNT_TITLE')}</span>
+                                <span className="tag--primary d--ib ml-2">{myReviews ? myReviews.count : 0}</span>
                             </div>
                             <div>
                                 {/* <span>Sort by</span>
@@ -139,7 +121,7 @@ const MyReviews = () => {
                                 <i className="icon icon--base icon--chevron-down icon--primary"></i> */}
                             </div>
                         </div>
-                        {myReviewsLoading ? (
+                        {isLoading ? (
                             <div>
                                 <LoaderMyReviews />
                                 <LoaderMyReviews />
@@ -150,13 +132,9 @@ const MyReviews = () => {
                                 {loadedReviews && loadedReviews.length > 0 ? (
                                     <>
                                         <div className="reviews-list">
-                                            {loadedReviews.map(
-                                                (item: IMyReview) => (
-                                                    <ReviewItem
-                                                        reviewItem={item}
-                                                    />
-                                                )
-                                            )}
+                                            {loadedReviews.map((item: IMyReview) => (
+                                                <ReviewItem reviewItem={item} />
+                                            ))}
                                         </div>
                                         {/* <Pagination
                                             activePageClass={
@@ -171,14 +149,8 @@ const MyReviews = () => {
                                 ) : (
                                     <div className="reviews-list">
                                         <div className="type--center mt-22">
-                                            <h1 className="type--xxl">
-                                                {t(
-                                                    'MY_REVIEWS.NO_RESULT.TITLE'
-                                                )}
-                                            </h1>
-                                            <p className="type--color--secondary">
-                                                {t('MY_REVIEWS.NO_RESULT.DESC')}
-                                            </p>
+                                            <h1 className="type--xxl">{t('MY_REVIEWS.NO_RESULT.TITLE')}</h1>
+                                            <p className="type--color--secondary">{t('MY_REVIEWS.NO_RESULT.DESC')}</p>
                                         </div>
                                     </div>
                                 )}
@@ -187,16 +159,12 @@ const MyReviews = () => {
                     </div>
                     <div className="my-reviews__aside">
                         <div className="mb-10">
-                            <span className="type--uppercase type--color--tertiary">
-                                {t('MY_REVIEWS.AVG_SCORE')}
-                            </span>
+                            <span className="type--uppercase type--color--tertiary">{t('MY_REVIEWS.AVG_SCORE')}</span>
                             <span className="tag--primary d--ib ml-2">
                                 {/* {getAvgRating(myReviews)
                                     ? getAvgRating(myReviews).toFixed(1)
                                     : 0} */}
-                                {tutorStatistics?.statistic
-                                    ? tutorStatistics?.statistic.toFixed(1)
-                                    : 0}
+                                {tutorStatistics?.statistic ? tutorStatistics?.statistic.toFixed(1) : 0}
                             </span>
                         </div>
                         {statisticsLoading ? (
@@ -204,13 +172,7 @@ const MyReviews = () => {
                                 <LoaderStatistics />
                             </div>
                         ) : (
-                            <Ratings
-                                ratings={
-                                    tutorStatistics
-                                        ? tutorStatistics.result
-                                        : []
-                                }
-                            />
+                            <Ratings ratings={tutorStatistics ? tutorStatistics.result : []} />
                         )}
                     </div>
                 </div>
