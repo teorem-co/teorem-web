d7a264ac769dc901b863d275c51c32214a3c3734
ivan.sakoman <ivan.sakoman@reroot.hr>
2022-03-18 11:37:49 +0100

    added chat

--- src/app/features/chat/pages/Chat.tsx
+++ src/app/features/chat/pages/Chat.tsx
@@ -1,14 +1,43 @@
+import { useEffect, useState } from 'react';
+import { ChatEngine } from 'react-chat-engine';
+
+import IChatEnginePost from '../../../../interfaces/IChatEnginePost';
+import IUser from '../../../../interfaces/IUser';
 import MainWrapper from '../../../components/MainWrapper';
 import { chatConversation, chatConversationList, IChatConversation, IChatConversationItem } from '../../../constants/chatConstants';
+import { useAppSelector } from '../../../hooks';
+import { useAddUserMutation } from '../../../services/chatEngineService';
 import ConversationAside from '../components/ConversationAside';
 import LoggedUserMessage from '../components/LoggedUserMessage';
 import OtherUserMessage from '../components/OtherUserMessage';
 
 const Chat = () => {
+    const user = useAppSelector((state) => state.auth.user);
+
+    const chatUserName = user?.email.split('@')[0];
+
+    const [addUserQuery] = useAddUserMutation();
+
+    const addUser = async () => {
+        const toSend: IChatEnginePost = {
+            email: 'parent@reroot.hr',
+            first_name: 'Parent',
+            last_name: 'Parentić',
+            secret: 'Password1!',
+            username: 'parent',
+        };
+
+        const res = await addUserQuery(toSend).unwrap();
+    };
+
     return (
         <MainWrapper>
+            <div>
+                <button onClick={() => addUser()}>add user</button>
+            </div>
             <div className="card--chat">
-                <div className="card--chat__aside">
+                <ChatEngine height="80vh" userName={chatUserName} userSecret="Password1!" projectID="118ac403-e935-406b-a5bd-3239a377af7b" />
+                {/* <div className="card--chat__aside">
                     <div className="p-4">
                         <div className="type--wgt--bold type--lg">Chat</div>
                         <input type="text" placeholder="Search in chat" className="input p-3 mt-6" />
@@ -19,7 +48,7 @@ const Chat = () => {
                         })}
                     </div>
                 </div>
-                {/* Active chat */}
+                
                 <div className="content">
                     <div className="content__header content__header--chat">
                         <div className="flex flex--center">
@@ -29,7 +58,7 @@ const Chat = () => {
                         </div>
                         <button className="btn btn--primary btn--base">Book a session</button>
                     </div>
-                    {/* Messages */}
+                    
                     <div className="content__main">
                         {chatConversation.map((chatConversation: IChatConversation, index: number) => {
                             return chatConversation.incomingMessage ? (
@@ -45,7 +74,7 @@ const Chat = () => {
                         </div>
                         <input type="text" className="input ml-5 p-2" />
                     </div>
-                </div>
+                </div> */}
             </div>
         </MainWrapper>
     );
---
+++ src/interfaces/IChatEnginePost.ts
@@ -0,0 +1,9 @@
+interface IChatEnginePost {
+    username: string;
+    secret: string;
+    email: string;
+    first_name: string;
+    last_name: string;
+}
+
+export default IChatEnginePost;
