6361fbb4270756f7059bca212990c431ca536881
Kresimir Roncevic <kresimir@koona.tech>
2022-06-21 00:33:42 +0200

    qf

--- src/app/features/register/EmailConfirmationModal.tsx
+++ src/app/features/register/EmailConfirmationModal.tsx
@@ -3,11 +3,26 @@ import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 
 import logo from '../../../assets/images/logo.svg';
+import { useResendActivationEmailMutation } from '../../../services/authService';
+import { useAppSelector } from '../../hooks';
 import { PATHS } from '../../routes';
 
 const EmailConfirmatioPopup = () => {
 
     const history = useHistory();
+    const [resendActivationEmailPost, { isSuccess: isSuccessResendActivationEmail }] = useResendActivationEmailMutation();
+    const email = useAppSelector((state) => state.tutorRegister.email);
+    const [disableButton, setDisableButton] = useState(false);
+    
+    const resendActivationEmail = () => {
+
+        if (email.length) {
+            resendActivationEmailPost({
+                email: email
+            });
+            setDisableButton(true);
+        }
+    };
 
     return (
         <>
@@ -17,11 +32,11 @@ const EmailConfirmatioPopup = () => {
                 </div>
                 <div className="type--lg type--wgt--bold mb-4">{t('EMAIL_CONFIRMATION_POPUP.WELCOME')}</div>
                 <div className="type--color--secondary mb-8 w--448--max">{t('EMAIL_CONFIRMATION_POPUP.DESCRIPTION')}</div>
-                <div className="btn btn--primary btn--lg mt-6 mb-4" onClick={() => console.log('click')}>
+                <div className="btn btn--primary btn--lg mt-6 mb-4" onClick={resendActivationEmail}>
                     {t('EMAIL_CONFIRMATION_POPUP.EXPIRATION')}
                 </div>
                 <div onClick={() =>{history.push(PATHS.LOGIN);}}>
-                    <button className="btn btn--base btn--clear">{t('EMAIL_CONFIRMATION_POPUP.CONFIRM_BUTTON')}</button>
+                    <button className={`btn btn--base btn--clear${disableButton && ' btn--disabled'}`}>{t('EMAIL_CONFIRMATION_POPUP.CONFIRM_BUTTON')}</button>
                 </div>
             </div>
             <div className="trial__overlay"></div>
