f8a629fa772d68e6a48595105a748cdea5b09026
Dima <dumitrucepoi03@gmail.com>
2023-04-20 18:56:32 +0100

    fix profile preview link on my profile page (tutors)

--- src/app/features/my-profile/components/ProfileHeader.tsx
+++ src/app/features/my-profile/components/ProfileHeader.tsx
@@ -1,35 +1,47 @@
 import { t } from 'i18next';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import { RoleOptions } from '../../../../slices/roleSlice';
 import { useAppSelector } from '../../../hooks';
 import { PATHS } from '../../../routes';
 import { getUserId } from '../../../utils/getUserId';
+import { useLazyGetTutorProfileDataQuery } from '../../../../services/tutorService';
 
 interface Props {
-    className?: string;
+  className?: string;
 }
 
 const ProfileHeader: FC<Props> = (props: Props) => {
-    const { className } = props;
+  const { className } = props;
 
-    const tutorId = getUserId();
-    const userRole: string = useAppSelector((state) => state.auth.user?.Role.abrv) || '';
+  const tutorId = getUserId();
+  const userRole: string = useAppSelector((state) => state.auth.user?.Role.abrv) || '';
 
-    return (
-        <div className={`flex--primary ${className}`}>
-            <div className="type--lg type--wgt--bold flex--grow">{t('MY_PROFILE.TITLE')}</div>
-            <div>
-                {/* Preview profile has to be div, because of formik validation bug */}
-                {userRole === RoleOptions.Tutor && (
-                    <Link to={`${PATHS.SEARCH_TUTORS_TUTOR_PROFILE.replace(":tutorSlug", `${tutorId}`)}`} className="btn btn--clear btn--base type--wgt--bold">
-                        {t('MY_PROFILE.PREVIEW')}
-                    </Link>
-                )}
-            </div>
-        </div>
-    );
+  const [getTutor, { data: tutorData }] = useLazyGetTutorProfileDataQuery();
+
+  useEffect(() => {
+    if (userRole === RoleOptions.Tutor && !!tutorId) {
+      getTutor(tutorId);
+    }
+  }, [userRole, tutorId]);
+
+  return (
+    <div className={`flex--primary ${className}`}>
+      <div className="type--lg type--wgt--bold flex--grow">{t('MY_PROFILE.TITLE')}</div>
+      <div>
+        {/* Preview profile has to be div, because of formik validation bug */}
+        {userRole === RoleOptions.Tutor && (
+          <Link
+            to={`${PATHS.SEARCH_TUTORS_TUTOR_PROFILE.replace(':tutorSlug', `${tutorData?.slug}`)}`}
+            className="btn btn--clear btn--base type--wgt--bold"
+          >
+            {t('MY_PROFILE.PREVIEW')}
+          </Link>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default ProfileHeader;
