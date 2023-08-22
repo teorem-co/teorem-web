217ba05161aa6381717c0e0de464aeff4c221ec1
antonio-code-of-us <135585663+antonio-code-of-us@users.noreply.github.com>
2023-08-22 14:32:52 +0200

    fix urls for meeting

--- src/app/services/hiLinkService.ts
+++ src/app/services/hiLinkService.ts
@@ -3,10 +3,10 @@ import IGetRoomLink from '../../interfaces/IGetRoomLink';
 import { baseService } from '../baseService';
 import { HttpMethods } from '../lookups/httpMethods';
 
-const URL = '/api/v1/meeting';
+const URL = '/api/v1/meetings';
 
 interface IMeetingUrl{
-    meetingUrl: string;
+  meetingUrl: string;
 }
 
 export interface IMeetRecording{
@@ -15,30 +15,30 @@ export interface IMeetRecording{
   videoTitle: string
 }
 export const hiLinkService = baseService.injectEndpoints({
-    endpoints: (builder) => ({
-        getRoomLink: builder.query<IMeetingUrl, IGetRoomLink>({
-            query: (params) => ({
-                url: `${URL}/get-room-link/?userId=${params.userId}&bookingId=${params.bookingId}`,
-                method: HttpMethods.GET,
-            }),
-        }),
-        getLessonRecordings: builder.query<IMeetRecording[], IGetRecordingLinks>({
-            query: (params) => ({
-                url: `${URL}/recordings?meetingId=${params.meetingId}`,
-                method: HttpMethods.GET,
-            }),
-        }),
-        getFreeConsultationLink: builder.query<string, string>({
-            query: (tutorId) => ({
-                url: `${URL}/free-consultation/${tutorId}`,
-                method: HttpMethods.GET,
-            }),
-        }),
+  endpoints: (builder) => ({
+    getRoomLink: builder.query<IMeetingUrl, IGetRoomLink>({
+      query: (params) => ({
+        url: `${URL}/room-link?userId=${params.userId}&bookingId=${params.bookingId}`,
+        method: HttpMethods.GET,
+      }),
     }),
+    getLessonRecordings: builder.query<IMeetRecording[], IGetRecordingLinks>({
+      query: (params) => ({
+        url: `${URL}/${params.meetingId}/recordings`,
+        method: HttpMethods.GET,
+      }),
+    }),
+    getFreeConsultationLink: builder.query<string, string>({
+      query: (tutorId) => ({
+        url: `${URL}/free-consultation/${tutorId}`,
+        method: HttpMethods.GET,
+      }),
+    }),
+  }),
 });
 
 export const {
-    useLazyGetRoomLinkQuery,
-    useLazyGetLessonRecordingsQuery,
-    useLazyGetFreeConsultationLinkQuery
+  useLazyGetRoomLinkQuery,
+  useLazyGetLessonRecordingsQuery,
+  useLazyGetFreeConsultationLinkQuery
 } = hiLinkService;
