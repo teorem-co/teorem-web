caf1dfc88f1c04739bd58cd444bcb03d999ab27c
SilvijaG1302 <119846820+SilvijaG1302@users.noreply.github.com>
2023-08-29 13:43:57 +0200

    TRM-79: date format (#40)

--- src/app/features/my-bookings/components/ParentEventModal.tsx
+++ src/app/features/my-bookings/components/ParentEventModal.tsx
@@ -83,7 +83,7 @@ const ParentEventModal: React.FC<IProps> = (props) => {
                         <div className="flex flex--center mb-4">
                             <i className="icon icon--base icon--subject icon--grey mr-4"></i>
                             <div className="type--color--secondary">
-                              {t(`SUBJECTS.${event.Subject.abrv.replace(' ', '').replace('-', '').toLowerCase()}`)} -
+                                {t(`SUBJECTS.${event.Subject.abrv}`)} -
                                 {event.Level.name === 'IB (International Baccalaurate)' ?
                                     <td>{t('LEVELS.ib')}</td> :
                                     <td>{t(`LEVELS.${event.Level.name.replace('-', '').replace(' ', '').toLowerCase()}`)}</td>
