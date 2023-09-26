c9fa03e7fe395c66b857b2db8e2428f3babc9103
antonio-code-of-us <135585663+antonio-code-of-us@users.noreply.github.com>
2023-09-26 14:07:06 +0200

    change threshold

--- src/app/features/my-bookings/MyBookings.tsx
+++ src/app/features/my-bookings/MyBookings.tsx
@@ -502,7 +502,7 @@ const MyBookings: React.FC = (props: any) => {
                scrollToTime={defaultScrollTime}
                showMultiDayTimes={true}
                step={15}
-               longPressThreshold={200}
+               longPressThreshold={700}
                selectable={true}
                timeslots={4}
                onSelectSlot={(e) => handleSelectedSlot(e)}
