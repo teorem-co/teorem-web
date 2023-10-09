a6627f3b61b16af2f4599efda7d349758b41dda2
SilvijaTeorem <silvija.gojevic@teorem.co>
2023-10-09 09:54:33 +0200

    stripe pci compliance

--- src/app/features/my-profile/components/AddCreditCard.tsx
+++ src/app/features/my-profile/components/AddCreditCard.tsx
@@ -6,6 +6,11 @@ import TextField from '../../../components/form/TextField';
 import MySelect from '../../../components/form/MySelectField';
 import { countryInput } from '../../../constants/countryInput';
 import { countryOption } from '../../../constants/countryOption';
+import {
+  CardNumberElement,
+  useElements,
+  useStripe
+} from "@stripe/react-stripe-js";
 
 interface Props {
   sideBarIsOpen: boolean;
@@ -28,6 +33,8 @@ export interface Values {
 
 const AddCreditCard = (props: Props) => {
   const { sideBarIsOpen, closeSidebar } = props;
+  const stripe = useStripe();
+  const elements = useElements();
 
   const countryOptions = [
     { label: 'Argentina', value: 'AR' },
@@ -101,7 +108,27 @@ const AddCreditCard = (props: Props) => {
     zipCode: '',
   };
 
-  const handleSubmit = (values: Values) => {
+  const handleSubmit = async (values: Values) => {
+    console.log("submit");
+    const card = elements?.create("card");
+    console.log("created card");
+    if(card == undefined) {
+      console.log("card undefined");
+      return;
+    }
+
+    console.log("create token");
+    const tokenResult = await stripe?.createToken(card, {
+      name: formik.values.cardFirstName + " " + formik.values.cardLastName,
+      address_line1: formik.values.line1,
+      address_line2: formik.values.line2,
+      address_city: formik.values.city,
+      address_zip: formik.values.zipCode,
+      address_country: formik.values.country,
+      currency: 'EUR',
+    });
+    console.log("token created");
+    console.log(tokenResult);
     props.handleSubmit(values);
   };
 
