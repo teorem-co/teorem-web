02e052a20089e06998d773451a72d26494abaae2
SilvijaTeorem <silvija.gojevic@teorem.co>
2023-10-09 11:52:53 +0200

    rollback to previous state

--- src/app/features/my-profile/components/AddCreditCard.tsx
+++ src/app/features/my-profile/components/AddCreditCard.tsx
@@ -6,7 +6,6 @@ import TextField from '../../../components/form/TextField';
 import MySelect from '../../../components/form/MySelectField';
 import { countryInput } from '../../../constants/countryInput';
 import { countryOption } from '../../../constants/countryOption';
-import {loadStripe} from "@stripe/stripe-js";
 
 interface Props {
   sideBarIsOpen: boolean;
@@ -29,8 +28,6 @@ export interface Values {
 
 const AddCreditCard = (props: Props) => {
   const { sideBarIsOpen, closeSidebar } = props;
-  const stripe = useStripe();
-  const elements = useElements();
 
   const countryOptions = [
     { label: 'Argentina', value: 'AR' },
@@ -104,27 +101,7 @@ const AddCreditCard = (props: Props) => {
     zipCode: '',
   };
 
-  const handleSubmit = async (values: Values) => {
-    console.log("submit");
-    const card = elements?.create("card");
-    console.log("created card");
-    if(card == undefined) {
-      console.log("card undefined");
-      return;
-    }
-
-    console.log("create token");
-    const tokenResult = await stripe?.createToken(card, {
-      name: formik.values.cardFirstName + " " + formik.values.cardLastName,
-      address_line1: formik.values.line1,
-      address_line2: formik.values.line2,
-      address_city: formik.values.city,
-      address_zip: formik.values.zipCode,
-      address_country: formik.values.country,
-      currency: 'EUR',
-    });
-    console.log("token created");
-    console.log(tokenResult);
+  const handleSubmit = (values: Values) => {
     props.handleSubmit(values);
   };
 
@@ -268,3 +245,4 @@ const AddCreditCard = (props: Props) => {
 };
 
 export default AddCreditCard;
+
--- src/app/features/my-profile/pages/ProfileAccount.tsx
+++ src/app/features/my-profile/pages/ProfileAccount.tsx
@@ -38,16 +38,12 @@ import {
 } from '../services/stripeService';
 import { setMyProfileProgress } from '../slices/myProfileSlice';
 import StripeConnectForm from '../components/StripeConnectForm';
-import {Elements, useElements} from "@stripe/react-stripe-js";
-import {loadStripe, StripeCardElement} from "@stripe/stripe-js";
 
 interface Values {
   currentPassword: string;
   newPassword: string;
   confirmPassword: string;
 }
-
-const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_API_KEY!);
 
 const ProfileAccount = () => {
   const [getProfileProgress] = useLazyGetProfileProgressQuery();
@@ -206,32 +202,12 @@ const ProfileAccount = () => {
       card: toSend,
     };
 
-    const cardInfo = {
-      number: toSend.number,
-      exp_month: toSend.exp_month,
-      exp_year: toSend.exp_year,
-      cvc: toSend.cvc
-    }
-
-    const stripe = await stripePromise;
-    const elements = useElements();
-    if(!stripe) return;
-    if(!elements) return;
-    var cardElement: StripeCardElement = elements.create('card');
-    stripe.createToken(cardElement, cardInfo)
-      .then((result) => {
-      if(result.error) {
-        console.error("error");
-      } else {
-        addCustomerSource(toSendCustomerSource)
-          .unwrap()
-          .then(() => {
-            fetchData();
-            closeAddCardSidebar();
-          });
-      }
-    });
-
+    addCustomerSource(toSendCustomerSource)
+      .unwrap()
+      .then(() => {
+        fetchData();
+        closeAddCardSidebar();
+      });
   };
 
   const handleDeleteCreditCard = async (cardId: string) => {
@@ -522,11 +498,10 @@ const ProfileAccount = () => {
           closeSidebar={() => setStripeModalOpen(false)}
         />
       </div>
-      <Elements stripe={stripePromise}>
-        <AddCreditCard handleSubmit={handleSubmitCreditCard} closeSidebar={closeAddCardSidebar} sideBarIsOpen={addSidebarOpen} />
-      </Elements>
+      <AddCreditCard handleSubmit={handleSubmitCreditCard} closeSidebar={closeAddCardSidebar} sideBarIsOpen={addSidebarOpen} />
     </MainWrapper>
   );
 };
 
 export default ProfileAccount;
+
