e8f54effe0e5f54ba42aaa4f5aa666da941a45be
ijuren <juren.ivan@gmail.com>
2023-10-26 12:08:54 +0200

    add console.logs to add clarity on dev

--- src/app/baseService.ts
+++ src/app/baseService.ts
@@ -5,8 +5,6 @@ import { BaseQueryApi } from '@reduxjs/toolkit/src/query/baseQueryTypes';
 import { logout, setToken } from '../slices/authSlice';
 
 
-let refreshInProgress: Promise<void> | null = null;
-
 const baseQuery = fetchBaseQuery({
   baseUrl: `${process.env.REACT_APP_SCHEMA}://${process.env.REACT_APP_HOST}:${process.env.REACT_APP_API_PORT}/`,
   credentials: 'include',
@@ -20,8 +18,11 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
+let refreshInProgress: Promise<void> | null = null;
+
 const baseQueryWithReauth = async (args: any, api: BaseQueryApi, extraOptions: any) => {
   if (refreshInProgress) {
+    console.log('Waiting for refresh token to complete.');
     // Wait for the refresh to complete
     await refreshInProgress;
   }
@@ -29,7 +30,9 @@ const baseQueryWithReauth = async (args: any, api: BaseQueryApi, extraOptions: a
   let result = await baseQuery(args, api, extraOptions);
 
   if (result?.error?.status === 403) {
+    console.log('403 detected.');
     if (!refreshInProgress) {
+      console.log('Should call refresh.');
       // Declare that a refresh is in progress
       let resolveRefresh: (() => void) | undefined;
       refreshInProgress = new Promise(resolve => {
@@ -39,6 +42,7 @@ const baseQueryWithReauth = async (args: any, api: BaseQueryApi, extraOptions: a
       const refreshResult = await baseQuery('/api/v1/auth/refresh', api, extraOptions);
 
       if (refreshResult?.data) {
+        console.log('Refresh token successful. Saving it in the store.');
         api.dispatch(setToken({
           // eslint-disable-next-line @typescript-eslint/ban-ts-comment
           // @ts-ignore
@@ -47,21 +51,26 @@ const baseQueryWithReauth = async (args: any, api: BaseQueryApi, extraOptions: a
           // @ts-ignore
           user: refreshResult?.data.user,
         }));
+        console.log('Retrying original request.');
         result = await baseQuery(args, api, extraOptions);
       } else {
+        console.log('Refresh token failed. Logging out.');
         api.dispatch(logout());
         window.location.href = `${window.location.origin}/en/login`;
       }
 
       // Declare end of refresh
       if (resolveRefresh) {
+        console.log('Refresh token complete.');
         resolveRefresh();
       }
 
       refreshInProgress = null;
     } else {
+      console.log('Refresh in progress. Waiting for it to complete.');
       // If another query is already refreshing the token, wait for it to finish
       await refreshInProgress;
+      console.log('Retrying original request.');
       result = await baseQuery(args, api, extraOptions);
     }
   }
