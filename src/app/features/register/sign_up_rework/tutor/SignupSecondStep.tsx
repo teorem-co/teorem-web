bde2acb4d73db421110261aac353d3b23d52a456
antonio-code-of-us <135585663+antonio-code-of-us@users.noreply.github.com>
2023-10-26 15:48:44 +0200

    separate mail and number validiation

--- src/app/features/register/sign_up_rework/tutor/SignupSecondStep.tsx
+++ src/app/features/register/sign_up_rework/tutor/SignupSecondStep.tsx
@@ -3,7 +3,7 @@ import * as Yup from 'yup';
 import { t } from 'i18next';
 import moment from 'moment';
 import MyPhoneInput from '../../../../components/form/MyPhoneInput';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import TextField from '../../../../components/form/TextField';
 import { useCheckMailMutation } from '../../../../../services/authService';
 import useOutsideAlerter from '../../../../utils/useOutsideAlerter';
@@ -27,41 +27,57 @@ export const SignupSecondStep = ({ nextStep }:StepTwoProps) => {
   const {email, phoneNumber} = state;
   const rangeSetterRef = useRef<HTMLDivElement>(null);
   const [checkMail] = useCheckMailMutation();
-   const [checkMailValidation, setCheckMailValidation] = useState<string>('');
-    const selectedRole = useAppSelector((state) => state.role.selectedRole);
+  const selectedRole = useAppSelector((state) => state.role.selectedRole);
 
-    //
-  const initialValues: StepTwoValues = {
+  const [emailV, setEmailV] = useState(email);
+  const [phoneNumberV, setPhoneNumberV] = useState(phoneNumber);
+
+  const initialValues1: any = {
     email: email,
+  };
+
+  const initialValues2: any = {
     phoneNumber: phoneNumber,
   };
 
   const formik = useFormik({
-    initialValues: initialValues,
-    onSubmit: (values) => handleSubmitStepTwo(values),
+    initialValues: initialValues1,
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    onSubmit: () => {},
     validateOnBlur: true,
     validateOnChange: false,
     validateOnMount:true,
     enableReinitialize: false,
     validationSchema: Yup.object().shape({
       email: Yup.string().email(t('FORM_VALIDATION.INVALID_EMAIL'))
-        .required(t('FORM_VALIDATION.REQUIRED'))
         .test('checkEmailExistence', t('REGISTER.FORM.EMAIL_CONFLICT'), async (value) => {
           if (!value) return true;  // If value is not present, return true to skip further validation
 
           const emailExists = await checkMail({ email: value }).unwrap();
           return !emailExists;
-        }),
+        })
+        .required(t('FORM_VALIDATION.REQUIRED')),
+    }),
+  });
 
+  const formik2 = useFormik({
+    initialValues: initialValues2,
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    onSubmit: () => {},
+    validateOnBlur: true,
+    validateOnChange: true,
+    validateOnMount:true,
+    enableReinitialize: false,
+    validationSchema: Yup.object().shape({
       phoneNumber: Yup.string()
         .required(t('FORM_VALIDATION.REQUIRED'))
+        .min(11)
         .matches(
           /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/gm,
           t('FORM_VALIDATION.PHONE_NUMBER')
         ),
     }),
   });
-
 
   const handleEnterKeyOne = (event: React.KeyboardEvent<HTMLFormElement>) => {
     if (event.key === 'Enter') {
@@ -70,19 +86,36 @@ export const SignupSecondStep = ({ nextStep }:StepTwoProps) => {
     }
   };
 
-  function handleSubmitStepTwo(values: StepTwoValues) {
-   dispatch(
-     setStepTwo(
-       {
-         email: values.email,
-         phoneNumber: values.phoneNumber,
-         countryId: 'da98ad50-5138-4f0d-b297-62c5cb101247' //TODO: figure out country id somehow, for now its HC to Croatia
-       }
-     )
-   );
+  useEffect(() => {
+    setEmailV(formik.values.email);
+  }, [formik.values.email]);
 
-   nextStep();
+  useEffect(() => {
+    setPhoneNumberV(formik2.values.phoneNumber);
+  }, [formik2.values.phoneNumber]);
+
+  function handleSubmitEmail(values: any) {
+   setEmailV(values);
   }
+
+  function handleSubmitNumber(values: any) {
+   setPhoneNumberV(values);
+  }
+
+
+  function handleSubmitStepTwo() {
+    if (formik.values.email && formik2.values.phoneNumber) {
+      dispatch(
+        setStepTwo({
+          email: formik.values.email,
+          phoneNumber: formik2.values.phoneNumber,
+          countryId: 'da98ad50-5138-4f0d-b297-62c5cb101247'
+        })
+      );
+      nextStep();
+    }
+  }
+
 
   return (
     <>
@@ -103,27 +136,36 @@ export const SignupSecondStep = ({ nextStep }:StepTwoProps) => {
               />
             </div>
 
+          </Form>
+        </FormikProvider>
+
+        <FormikProvider value={formik2}>
+          <Form onKeyPress={handleEnterKeyOne}>
+
             {/*phone number*/}
             <div className="align--center mb-5" ref={rangeSetterRef}>
               <MyPhoneInput
-                form={formik}
+                form={formik2}
                 name="phoneNumber"
-                field={formik.getFieldProps('phoneNumber')}
-                meta={formik.getFieldMeta('phoneNumber')}
+                field={formik2.getFieldProps('phoneNumber')}
+                meta={formik2.getFieldMeta('phoneNumber')}
               />
               <div className="password-tooltip text-align--center info-text">{t('REGISTER.FORM.PHONE_INFO')}</div>
 
             </div>
-
-            <button
-              disabled={!formik.isValid}
-              id={`next-button-second-step-${selectedRole}`}
-              type="button"
-              className="btn btn--lg btn--primary cur--pointer mt-5 btn-signup"
-              // style={{borderRadius:"10px", fontWeight:'bolder'}}
-              onClick={() => formik.handleSubmit()}>{t('REGISTER.NEXT_BUTTON')}</button>
           </Form>
         </FormikProvider>
+
+        <button
+          disabled={!formik.isValid || !formik2.isValid}
+          id={`next-button-second-step-${selectedRole}`}
+          type="button"
+          className="btn btn--lg btn--primary cur--pointer mt-5 btn-signup"
+          onClick={() => handleSubmitStepTwo()}
+        >
+          {t('REGISTER.NEXT_BUTTON')}
+        </button>
+
       </div>
     </>
   );
