cd03152d688e4579efc8dcb9083ec574c8f0e448
SilvijaTeorem <silvija.gojevic@teorem.co>
2023-10-30 14:53:50 +0100

    Download icon

--- src/app/features/earnings/PayoutsTableElement.tsx
+++ src/app/features/earnings/PayoutsTableElement.tsx
@@ -1,60 +1,53 @@
+import {t} from 'i18next';
+import React, {useState} from 'react';
 import {
-  CategoryScale,
-  Chart as ChartJS,
-  Filler,
-  Legend,
-  LinearScale,
-  LineElement,
-  PointElement,
-  Title,
-  Tooltip,
-} from 'chart.js';
-import { t } from 'i18next';
-import moment from 'moment';
-import React, { useEffect, useState } from 'react';
-import { Line } from 'react-chartjs-2';
-import Select from 'react-select';
-
-import { OptionType } from '../../components/form/MySelectField';
-import MainWrapper from '../../components/MainWrapper';
-import { calcYears } from '../../utils/yearOptions';
-import earningsGraphOptions from './constants/earningsGraphOptions';
-import IGraph from './interfaces/IGraph';
-import { useLazyGetEarningsQuery } from './services/earningsService';
-import {
-  Accordion, AccordionDetails,
+  Accordion,
+  AccordionDetails,
   AccordionSummary,
-  ToggleButton,
-  ToggleButtonGroup, Typography
+  Typography
 } from "@mui/material";
-import {
-  useLazyGetCompletedLessonsQuery
-} from "../my-bookings/services/completedLessonsService";
+
 interface PayoutsProps {
   month: string,
   bookingsNum: number,
   studentsNum: number,
   reviewsNum: number,
   revenue: number
-};
+}
 
 const PayoutsTableElement = (props: PayoutsProps) => {
+  const [accordion, setAccordion] = useState(false);
+
+  const changeAccordion = () => {
+    if(accordion) setAccordion(false);
+    else setAccordion(true);
+  };
+
   return (
     <>
       <td>
-        <Accordion style={{ border: 'none', boxShadow: 'none', backgroundColor: 'transparent', width: 'fit-content'}}>
+        <Accordion style={{
+          border: 'none',
+          boxShadow: 'none',
+          backgroundColor: 'transparent',
+          width: 'fit-content'
+        }}>
           <AccordionSummary>
-            <div style={{display: "flex", alignItems: "center"}}>
+            <div style={{display: "flex", alignItems: "center"}} onClick={() => changeAccordion()}>
               <i
                 id="letter"
-                className="icon icon--sm icon--chevron-right icon--grey mr-3"
+                className={`icon icon--sm icon--chevron-right icon--grey mr-3 ${accordion && 'rotate--90'}`}
               ></i>
-              <Typography style={{fontFamily: "Lato"}}>{props.month}</Typography>
+              <Typography
+                style={{fontFamily: "Lato"}}>{props.month}</Typography>
             </div>
           </AccordionSummary>
-          <AccordionDetails style={{overflow: 'hidden', whiteSpace: 'nowrap',}}>
+          <AccordionDetails style={{overflow: 'hidden', whiteSpace: 'nowrap'}}>
             <Typography style={{fontFamily: "Lato"}}>
-              {props.month}
+              <div style={{display: "flex", alignItems: "center"}}>
+                {props.month}
+                <i className="icon icon--base icon--download icon--primary"></i>
+              </div>
             </Typography>
           </AccordionDetails>
         </Accordion>
