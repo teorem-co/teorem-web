ce5d0f6b07f6deb60f436f8ba08f7bac53b93d86
antonio-code-of-us <135585663+antonio-code-of-us@users.noreply.github.com>
2023-11-14 13:40:33 +0100

    conditionally show modals for intro

---
+++ src/app/features/my-profile/components/HiLinkModalForTutorIntro.tsx
@@ -0,0 +1,20 @@
+
+interface Props{
+  handleClose: () => void;
+  roomLink: string;
+}
+
+export const HiLinkModalForTutorIntro = (props: Props) => {
+  const {handleClose, roomLink} = props;
+
+
+  return (
+    <>
+      <div className="iframe-modal">
+        <img src='/logo-purple-text.png' alt='' className="pos--abs ml-2 mt-5 iframe-logo" height='40px'/>
+        <i className="icon icon--base icon--close modal__close cur--pointer mt-2 mr-2" onClick={handleClose}></i>
+          <iframe style={{ width: '100%', height:'100%' }} id="frame" src={roomLink!} allow="camera;microphone"></iframe>
+      </div>
+    </>
+  );
+};
