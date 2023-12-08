d97e8386cf8548db9a90120f2344f51d970d3563
Antonio Bukovac <135585663+antonio-bukovac@users.noreply.github.com>
2023-12-08 15:09:43 +0100

    Fix screenshare, availability update,  (#147)
    Co-authored-by: antonio-code-of-us <135585663+antonio-code-of-us@users.noreply.github.com>
    Co-authored-by: SilvijaTeorem <silvija.gojevic@teorem.co>
    Co-authored-by: SilvijaG1302 <119846820+SilvijaG1302@users.noreply.github.com>

--- src/app/features/my-profile/components/HiLinkModalForTutorIntro.tsx
+++ src/app/features/my-profile/components/HiLinkModalForTutorIntro.tsx
@@ -13,7 +13,7 @@ export const HiLinkModalForTutorIntro = (props: Props) => {
       <div className="iframe-modal">
         <img src='/logo-purple-text.png' alt='' className="pos--abs ml-2 mt-5 iframe-logo" height='40px'/>
         <i className="icon icon--base icon--close modal__close cur--pointer mt-2 mr-2" onClick={handleClose}></i>
-          <iframe style={{ width: '100%', height:'100%' }} id="frame" src={roomLink!} allow="camera;microphone"></iframe>
+          <iframe style={{ width: '100%', height:'100%' }} id="frame" src={roomLink!} allow="camera;microphone;display-capture"></iframe>
       </div>
     </>
   );
