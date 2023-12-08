9c6a452a2553cbfa69f9e257ab4d5a53e8c4f48a
Antonio Bukovac <135585663+antonio-bukovac@users.noreply.github.com>
2023-12-08 14:05:04 +0100

    Add display-capture to allow in iframe (#146)
    Co-authored-by: antonio-code-of-us <135585663+antonio-code-of-us@users.noreply.github.com>

--- src/app/features/my-profile/components/LearnCubeModal.tsx
+++ src/app/features/my-profile/components/LearnCubeModal.tsx
@@ -45,7 +45,7 @@ const LearnCubeModal = (props: Props) => {
             <img src='/logo-purple-text.png' alt='' className="pos--abs ml-2 mt-5 iframe-logo" height='40px'/>
             <i className="icon icon--base icon--close modal__close cur--pointer mt-2 mr-2" onClick={handleClose}></i>
               {(isLoading && <LoaderPrimary />) || (
-                  <iframe style={{ width: '100%', height:'100%' }} id="frame" src={roomLink!} allow="camera;microphone"></iframe>
+                  <iframe style={{ width: '100%', height:'100%' }} id="frame" src={roomLink!} allow="camera;microphone;display-capture"></iframe>
               )}
           </div>
         </>
