465096d9c06529d31cac5eeab2d5d84a6b297773
SilvijaG1302 <silvija@codeofus.com>
2023-12-12 16:36:42 +0100

    Remove console log

--- src/app/features/earnings/PayoutsTableElement.tsx
+++ src/app/features/earnings/PayoutsTableElement.tsx
@@ -35,7 +35,6 @@ const PayoutsTableElement = (props: PayoutsProps) => {
       },
     })
       .then(response => {
-        console.log(response);
         if (response.ok) {
           return response.blob();
         } else {
