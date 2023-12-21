70f46e6668f32834b8e0b2b124ed760147d6e317
antonio-code-of-us <135585663+antonio-code-of-us@users.noreply.github.com>
2023-12-21 14:40:33 +0100

    fix text on cards and subjects list

--- src/app/features/searchTutors/components/CustomSubjectList.tsx
+++ src/app/features/searchTutors/components/CustomSubjectList.tsx
@@ -1,65 +1,86 @@
 import React, { useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import ISubject from '../../../../interfaces/ISubject';
 import useOutsideAlerter from '../../../utils/useOutsideAlerter';
+import { useAppSelector } from '../../../hooks';
+import {
+  allActiveSubjects,
+} from '../../register/sign_up_rework/student_and_parent/subjects';
 
 interface Props {
-    subjects: string[];
+  subjects: string[];
+  numOfSubjectsShown?: number;
 }
 
+/**
+ * CustomSubjectListLonger Component
+ * Displays a list of subjects.
+ *
+ * @param {string[]} subjects - The list of subjects to display.
+ * @param {number} [numOfSubjectsShowed=3] - The number of subjects to show initially. Default is 3. Others will be shown like +X
+ */
 const CustomSubjectList = (props: Props) => {
-    const { subjects } = props;
+  const { subjects, numOfSubjectsShown= 3 } = props;
+  const { t } = useTranslation();
+  const [showTooltip, setShowTooltip] = useState(false);
 
-    const { t } = useTranslation();
-    const [showTooltip, setShowTooltip] = useState(false);
+  const rangeSetterRef = useRef<HTMLDivElement>(null);
 
-    const rangeSetterRef = useRef<HTMLDivElement>(null);
+  const hideTooltip = () => {
+    setShowTooltip(false);
+  };
 
-    const hideTooltip = () => {
-        setShowTooltip(false);
-    };
+  useOutsideAlerter(rangeSetterRef, hideTooltip);
 
-    useOutsideAlerter(rangeSetterRef, hideTooltip);
+  const filtersState = useAppSelector((state) => state.searchFilters);
+  const { subject: subjectInFilter } = filtersState;
 
-    return (
-        <div ref={rangeSetterRef} className="flex flex--wrap flex--ai--center">
-            {subjects.slice(0, 2).map((subject) => (
-                <span className="tag tag--primary" key={subject}>
+  const sortedSubjects = subjects.slice().sort((a, b) => {
+    if (subjectInFilter) {
+      if (a === allActiveSubjects.find(sub => sub.id == subjectInFilter)?.abrv) return -1;
+      if (b === allActiveSubjects.find(sub => sub.id == subjectInFilter)?.abrv) return 1;
+    }
+    return a.localeCompare(b);
+  });
+
+  return (
+    <div ref={rangeSetterRef} className='flex flex--wrap flex--ai--center'>
+      {sortedSubjects.slice(0, numOfSubjectsShown).map((subject) => (
+        <span className='tag tag--primary' key={subject}>
                     {t(`SUBJECTS.${subject.replaceAll('-', '')}`)}
                 </span>
-            ))}
+      ))}
 
-            {subjects.length > 2 ? (
-                <div className="pos--rel">
+      {sortedSubjects.length > numOfSubjectsShown ? (
+        <div className='pos--rel'>
                     <span
-                        className="type--color--brand cur--pointer"
-                        onClick={() => setShowTooltip(!showTooltip)}
+                      className='type--color--brand cur--pointer'
+                      onClick={() => setShowTooltip(!showTooltip)}
                     >
-                        {`+ ${subjects.length - 2} ${t(
-                            'SEARCH_TUTORS.SUBJECT_LIST.MORE'
+                        {`+ ${sortedSubjects.length - numOfSubjectsShown} ${t(
+                          'SEARCH_TUTORS.SUBJECT_LIST.MORE',
                         )}`}{' '}
                     </span>
-                    <div
-                        className={`tooltip--text ${
-                            showTooltip ? 'active' : ''
-                        }`}
-                    >
-                        {subjects.slice(2).map((subject) => (
-                            <span
-                                className="type--color--brand"
-                                key={subject}
-                            >
-                                {t(`SUBJECTS.${subject.replaceAll('-', '').replace(' ', '').toLowerCase()}`)}
-                            </span>
-                        ))}
-                    </div>
-                </div>
-            ) : (
-                <></>
-            )}
+          <div
+            className={`tooltip--text ${
+              showTooltip ? 'active' : ''
+            }`}
+          >
+            {sortedSubjects.slice(numOfSubjectsShown).map((subject) => (
+              <span
+                className='type--color--brand'
+                key={subject}
+              >
+                {t(`SUBJECTS.${subject.replaceAll('-', '').replace(' ', '').toLowerCase()}`)}
+              </span>
+            ))}
+          </div>
         </div>
-    );
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 };
 
 export default CustomSubjectList;
