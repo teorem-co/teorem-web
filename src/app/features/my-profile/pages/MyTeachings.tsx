a9fcaced90fff2cbbb527f31e0fe8d196fad47d7
Antonio Bukovac <135585663+antonio-bukovac@users.noreply.github.com>
2024-01-16 13:35:56 +0100

    Add links for help center and redesign subjects page (#176)
    Co-authored-by: antonio-code-of-us <135585663+antonio-code-of-us@users.noreply.github.com>

--- src/app/features/my-profile/pages/MyTeachings.tsx
+++ src/app/features/my-profile/pages/MyTeachings.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useHistory } from 'react-router-dom';
 
 import {
-  useLazyGetProfileProgressQuery, useLazyGetTutorByIdQuery,
+  useLazyGetProfileProgressQuery,
+  useLazyGetTutorByIdQuery,
 } from '../../../../services/tutorService';
 import MainWrapper from '../../../components/MainWrapper';
 import LoaderPrimary from '../../../components/skeleton-loaders/LoaderPrimary';
@@ -13,122 +14,101 @@ import AddSubjectSidebar from '../components/AddSubjectSidebar';
 import EditSubjectSidebar from '../components/EditSubjectSidebar';
 import ProfileCompletion from '../components/ProfileCompletion';
 import ProfileHeader from '../components/ProfileHeader';
-import SubjectList from '../components/SubjectList';
 import { setMyProfileProgress } from '../slices/myProfileSlice';
+import SubjectsPageMyTeachings from '../components/SubjectsPageMyTeachings';
 
 const MyTeachings = () => {
-    const [getProfileProgress] = useLazyGetProfileProgressQuery();
-    const [getProfileData, { data: myTeachingsData, isLoading: myTeachingsLoading, isUninitialized: myTeachingsUninitialized }] =
-        useLazyGetTutorByIdQuery();
+  const [getProfileProgress] = useLazyGetProfileProgressQuery();
+  const [getProfileData, {
+    data: myTeachingsData,
+    isLoading: myTeachingsLoading,
+    isUninitialized: myTeachingsUninitialized,
+  }] =
+    useLazyGetTutorByIdQuery();
 
-    const [addSidebarOpen, setAddSidebarOpen] = useState(false);
-    const [editSidebarOpen, setEditSidebarOpen] = useState(false);
+  const [addSidebarOpen, setAddSidebarOpen] = useState(false);
+  const [editSidebarOpen, setEditSidebarOpen] = useState(false);
 
-    const dispatch = useAppDispatch();
-    const profileProgressState = useAppSelector((state) => state.myProfileProgress);
-    const tutorId = getUserId();
-    const [currency, setCurrency] = useState('');
-    const { t } = useTranslation();
-    const isLoading = myTeachingsLoading || myTeachingsUninitialized;
-    const history = useHistory();
+  const dispatch = useAppDispatch();
+  const profileProgressState = useAppSelector((state) => state.myProfileProgress);
+  const tutorId = getUserId();
+  const { t } = useTranslation();
+  const isLoading = myTeachingsLoading || myTeachingsUninitialized;
+  const history = useHistory();
 
-    const closeAddSubjectSidebar = () => {
-        setAddSidebarOpen(false);
-    };
+  const closeAddSubjectSidebar = () => {
+    setAddSidebarOpen(false);
+  };
 
-    const closeEditSubjectSidebar = () => {
-        // history.location.search = '';
-        history.push(t('PATHS.PROFILE_PATHS.MY_PROFILE_INFO_TEACHINGS'));
-        setEditSidebarOpen(false);
-    };
+  const closeEditSubjectSidebar = () => {
+    // history.location.search = '';
+    history.push(t('PATHS.PROFILE_PATHS.MY_PROFILE_INFO_TEACHINGS'));
+    setEditSidebarOpen(false);
+  };
 
-    const handleSendId = (subjectId: string) => {
-        history.push(`?subjectId=${subjectId}`);
-        setEditSidebarOpen(true);
-    };
 
-    const fetchData = async () => {
-        if (tutorId) {
-            getProfileData(tutorId);
+  const fetchData = async () => {
+    if (tutorId) {
+      getProfileData(tutorId);
+      //If there is no state in redux for profileProgress fetch data and save result to redux
+      if (profileProgressState.percentage === 0) {
+        const progressResponse = await getProfileProgress().unwrap();
+        dispatch(setMyProfileProgress(progressResponse));
+      }
+    }
+  };
 
-            const tutorCurrency = await (await getProfileData(tutorId).unwrap()).User.Country.currencyCode;
-            setCurrency(tutorCurrency);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-            //If there is no state in redux for profileProgress fetch data and save result to redux
-            if (profileProgressState.percentage === 0) {
-                const progressResponse = await getProfileProgress().unwrap();
-                dispatch(setMyProfileProgress(progressResponse));
-            }
-        }
-    };
+  return (
+    <MainWrapper>
+      <div className='card--profile'>
+        {/* HEADER */}
+        <ProfileHeader className='mb-1' />
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+        {/* PROGRESS */}
+        <ProfileCompletion
+          generalAvailability={profileProgressState.generalAvailability}
+          additionalInformation={profileProgressState.aboutMe}
+          myTeachings={profileProgressState.myTeachings}
+          percentage={profileProgressState.percentage}
+          payment={profileProgressState.payment}
+        />
 
-    return (
-        <MainWrapper>
-            <div className="card--profile">
-                {/* HEADER */}
-                <ProfileHeader className="mb-1" />
+        {/* MY TEACHINGS */}
+        {(isLoading && <LoaderPrimary />) || (
+          <div className='card--profile__section'>
+            {/* Add subject */}
+            <div>
 
-                {/* PROGRESS */}
-                <ProfileCompletion
-                    generalAvailability={profileProgressState.generalAvailability}
-                    additionalInformation={profileProgressState.aboutMe}
-                    myTeachings={profileProgressState.myTeachings}
-                    percentage={profileProgressState.percentage}
-                    payment={profileProgressState.payment}
-                />
+              <div className='mb-2 type--wgt--bold'>
+                {t('MY_PROFILE.MY_TEACHINGS.TITLE')}
+              </div>
 
-                {/* MY TEACHINGS */}
-                {(isLoading && <LoaderPrimary />) || (
-                    <div className="card--profile__section">
-                        {/* Add subject */}
-                        <div>
-                            <div className="mb-2 type--wgt--bold">{t('MY_PROFILE.MY_TEACHINGS.TITLE')}</div>
-                            <div className="type--color--tertiary w--200--max">{t('MY_PROFILE.MY_TEACHINGS.DESCRIPTION')}</div>
-                        </div>
-                        <div>
-                            <div className="dash-wrapper dash-wrapper--adaptive flex--grow">
-                              {/* Map through subjects here */}
-                              {/* Test fields */}
-                              <SubjectList
-                                handleSendId={handleSendId}
-                                tutorSubjects={myTeachingsData && myTeachingsData.TutorSubjects ? myTeachingsData.TutorSubjects : []}
-                                currency={currency}
-                                key={myTeachingsData && myTeachingsData.TutorSubjects.length}
-                              />
-                                <div className="dash-wrapper__item">
-                                    <div className="dash-wrapper__item__element" onClick={() => setAddSidebarOpen(true)}>
-                                        <div className="flex--primary cur--pointer">
-                                            <div>
-                                                <div className="type--wgt--bold">{t('MY_PROFILE.MY_TEACHINGS.ADD_NEW')}</div>
-                                            </div>
-                                            <div>
-                                                <i className="icon icon--base icon--plus icon--primary"></i>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                )}
+              <div className='type--color--tertiary w--200--max'>
+                {t('MY_PROFILE.MY_TEACHINGS.DESCRIPTION')}
+              </div>
+              <SubjectsPageMyTeachings />
+
             </div>
-            <EditSubjectSidebar
-                sideBarIsOpen={editSidebarOpen}
-                closeSidebar={closeEditSubjectSidebar}
-                handleGetData={() => getProfileData(tutorId ? tutorId : '')}
-            />
-            <AddSubjectSidebar
-                key={myTeachingsData?.TutorSubjects.length}
-                sideBarIsOpen={addSidebarOpen}
-                closeSidebar={closeAddSubjectSidebar}
-                handleGetData={() => getProfileData(tutorId ? tutorId : '')}
-            />
-        </MainWrapper>
-    );
+          </div>
+        )}
+      </div>
+      <EditSubjectSidebar
+        sideBarIsOpen={editSidebarOpen}
+        closeSidebar={closeEditSubjectSidebar}
+        handleGetData={() => getProfileData(tutorId ? tutorId : '')}
+      />
+      <AddSubjectSidebar
+        key={myTeachingsData?.TutorSubjects.length}
+        sideBarIsOpen={addSidebarOpen}
+        closeSidebar={closeAddSubjectSidebar}
+        handleGetData={() => getProfileData(tutorId ? tutorId : '')}
+      />
+    </MainWrapper>
+  );
 };
 
 export default MyTeachings;
