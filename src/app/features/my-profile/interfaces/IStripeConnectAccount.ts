5a3a2492eb0afa1e9384eae57c7ac07cc7a0909a
Antonio Bukovac <135585663+antonio-bukovac@users.noreply.github.com>
2024-04-16 16:30:43 +0200

    TRM-237: new fields for US users (#237)
    Co-authored-by: antonio-code-of-us <135585663+antonio-code-of-us@users.noreply.github.com>
    Co-authored-by: SilvijaTeorem <silvija.gojevic@teorem.co>
    Co-authored-by: SilvijaG1302 <119846820+SilvijaG1302@users.noreply.github.com>
    Co-authored-by: SilvijaG1302 <silvija@codeofus.com>

--- src/app/features/my-profile/interfaces/IStripeConnectAccount.ts
+++ src/app/features/my-profile/interfaces/IStripeConnectAccount.ts
@@ -2,10 +2,14 @@ interface IStripeConnectAccount {
     refreshUrl?: string;
     returnUrl?: string;
     userId: string;
+    accountNumber?: string;
+    routingNumber?: string;
     IBAN?: string;
     IBANConfirm?: string;
     addressLine1?: string;
     addressLine2?: string;
+    state?: string;
+    last4SSN?: string;
     city?: string;
     country?: string;
     postalCode?: string;
