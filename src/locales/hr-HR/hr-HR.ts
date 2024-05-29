596cb284c1b9353c7272b97553ddaae97e7eb61f
Antonio Bukovac <135585663+antonio-bukovac@users.noreply.github.com>
2024-05-29 15:26:37 +0200

    TRM-246: add note for adding card (#260)
    Co-authored-by: Antonio Bukovac <135585663+antonio-code-of-us@users.noreply.github.com>

--- src/locales/hr-HR/hr-HR.ts
+++ src/locales/hr-HR/hr-HR.ts
@@ -665,6 +665,7 @@ export const HR_HR = {
             DESCRIPTION_TUTOR: 'Odaberi kako zeliš biti plaćen za instrukcije.',
             ADD_NEW: 'Dodaj novu karticu',
             ADD_NEW_DESC: 'Izaberi za dodavanje nove kartice',
+            NOTE: 'Nakon dodavanja kartice, slijedi proces verifikacije koji uključuje privremenu naplatu od €0.00.',
         },
         NEW_CARD: {
             ADD: 'DODAJTE NOVU KARTICU',
