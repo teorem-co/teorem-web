8eddfa28d97937a26bf5b094d44ac9cacb48889a
George2f <juraj.pejnovic@2front.co>
2024-08-21 18:18:19 +0200

    feat: make Sidebar usage more liberal

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,34 +1,35 @@
-import { useCallback, useEffect } from 'react';
+import { ReactNode, useCallback, useEffect } from 'react';
 
-interface Props {
-    children: JSX.Element | JSX.Element[];
-    sideBarIsOpen: boolean;
-    title: string;
-    closeSidebar: () => void;
-    onSubmit: () => void;
+interface ISidebarProps {
+    children?: ReactNode;
+    sideBarIsOpen?: boolean;
+    title?: string;
+    closeSidebar?: () => void;
+    onSubmit?: () => void;
     onCancel?: () => void;
-    submitLabel: string;
-    cancelLabel: string;
+    submitLabel?: string;
+    cancelLabel?: string;
 }
 
-const Sidebar = (props: Props) => {
-    const {
-        sideBarIsOpen,
-        title,
-        children,
-        closeSidebar,
-        onSubmit,
-        onCancel,
-        submitLabel,
-        cancelLabel,
-    } = props;
-
-    const escFunction = useCallback((event) => {
-        if (event.keyCode === 27) {
-            //Do whatever when esc is pressed
-            closeSidebar();
-        }
-    }, []);
+export default function Sidebar({
+    sideBarIsOpen,
+    title,
+    children,
+    closeSidebar,
+    onSubmit,
+    onCancel,
+    submitLabel,
+    cancelLabel,
+}: ISidebarProps) {
+    const escFunction = useCallback(
+        (event) => {
+            if (event.keyCode === 27) {
+                //Do whatever when esc is pressed
+                closeSidebar?.();
+            }
+        },
+        [closeSidebar]
+    );
 
     useEffect(() => {
         document.addEventListener('keydown', escFunction, false);
@@ -36,14 +37,12 @@ const Sidebar = (props: Props) => {
         return () => {
             document.removeEventListener('keydown', escFunction, false);
         };
-    }, []);
+    }, [escFunction]);
 
     return (
         <div className="pos--abs">
             <div
-                className={`cur--pointer sidebar__overlay ${
-                    !sideBarIsOpen ? 'sidebar__overlay--close' : ''
-                }`}
+                className={`cur--pointer sidebar__overlay ${!sideBarIsOpen ? 'sidebar__overlay--close' : ''}`}
                 onClick={closeSidebar}
             ></div>
 
@@ -55,32 +54,28 @@ const Sidebar = (props: Props) => {
                 <div className="flex--primary flex--shrink">
                     <div className="type--color--secondary">{title}</div>
                     <div>
-                        <i
-                            className="icon icon--base icon--close icon--grey"
-                            onClick={closeSidebar}
-                        ></i>
+                        <i className="icon icon--base icon--close icon--grey" onClick={closeSidebar}></i>
                     </div>
                 </div>
                 <div className="flex--grow mt-10">{children}</div>
                 <div className="flex--shirnk sidebar--secondary__bottom mt-10">
                     <div className="flex--primary mt-6">
-                        <button 
-                            className="btn btn--clear type--wgt--bold"
-                            onClick={onSubmit}
-                        >
-                            {submitLabel}
-                        </button>
-                        <button
-                            className="btn btn--clear type--color--error type--wgt--bold"
-                            onClick={onCancel ? onCancel : closeSidebar}
-                        >
-                            {cancelLabel}
-                        </button>
+                        {onSubmit ? (
+                            <button className="btn btn--clear type--wgt--bold" onClick={onSubmit}>
+                                {submitLabel}
+                            </button>
+                        ) : null}
+                        {onCancel || closeSidebar ? (
+                            <button
+                                className="btn btn--clear type--color--error type--wgt--bold"
+                                onClick={onCancel ? onCancel : closeSidebar}
+                            >
+                                {cancelLabel}
+                            </button>
+                        ) : null}
                     </div>
                 </div>
             </div>
         </div>
     );
-};
-
-export default Sidebar;
+}
